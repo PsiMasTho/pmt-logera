1b4134507d5b14d6db757b9ec143f312f90f9fb6
PsiMasTho <1cbb875@gmail.com>
2022-09-25 10:15:30 +0200

    tabs

--- src/fmt/fmt_specifiers.h
+++ src/fmt/fmt_specifiers.h
@@ -2,16 +2,16 @@
 #define INCLUDED_FMT_SPECIFIERS_H
 
 enum FormatSpecifier : int
-{			  // notations
+{             // notations
 	NUMBER,
-    SETSREPS, // xsyr OR r, r, r, ...
-    WEIGHT,	  // xx kg OR xx lb
-    WORD,
-    NONE,
-    TIME,	  // ONLY 24h
-    DURATION, // ONLY dd:hh:mm:ss.xxx
-    DISTANCE, // xxx meter OR  xxx kilometer OR xxx mile OR xxx yard
-    ENERGY	  // kcal OR kJ
+	SETSREPS, // xsyr OR r, r, r, ...
+	WEIGHT,	  // xx kg OR xx lb
+	WORD,
+	NONE,
+	TIME,	  // ONLY 24h
+	DURATION, // ONLY dd:hh:mm:ss.xxx
+	DISTANCE, // xxx meter OR  xxx kilometer OR xxx mile OR xxx yard
+	ENERGY	  // kcal OR kJ
 };
 
 #endif
