d1bc121ed98a615a7259b0fa1b33b01ea871b6a4
pmt <1cbb875@gmail.com>
2023-11-07 23:49:58 +0100

    Improvements to parsing and lexing specifications

--- tools/run_bisonc++.sh
+++ tools/run_bisonc++.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd "../src/" &&\
-bisonc++ ./log_parser_grammar    --filenames=log_parser    --no-lines --class-name log_parser    --token-class=log_tokens    --token-path ./log_token_enums.h &&\
-bisonc++ ./header_parser_grammar --filenames=header_parser --no-lines --class-name header_parser --token-class=header_tokens --token-path ./header_token_enums.h &&\
+bisonc++ ./log_parser_grammar    --filenames=log_parser    --no-lines --class-name=log_parser    --token-class=log_tokens    --token-path ./log_token_enums.h &&\
+#bisonc++ ./header_parser_grammar --filenames=header_parser --no-lines --class-name header_parser --token-class=header_tokens --token-path ./header_token_enums.h &&\
 echo SUCCESS
--- tools/run_flexc++.sh
+++ tools/run_flexc++.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd "../src/" &&\
-flexc++ log_scanner_lexer    --lex-source=log_scanner_lex.cc    --filenames=log_scanner    --no-lines --class-name log_scanner &&\
-flexc++ header_scanner_lexer --lex-source=header_scanner_lex.cc --filenames=header_scanner --no-lines --class-name header_scanner &&\
+flexc++ log_scanner_lexer    --no-lines &&\
+flexc++ header_scanner_lexer --no-lines &&\
 echo SUCCESS
