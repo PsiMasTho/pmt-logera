b97b2bdde5d6a6b9fa5e586256d7514d5902b8ac
pmt <1cbb875@gmail.com>
2023-11-08 00:51:00 +0100

    Added license text to test source files

--- test/sparse_array_test.cc
+++ test/sparse_array_test.cc
@@ -1,3 +1,7 @@
+//          Copyright (C) 2022 PsiMasTho (1cbb875@gmail.com)
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
 
 #include <catch2/catch_test_macros.hpp>
 
